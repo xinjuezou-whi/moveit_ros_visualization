945ba4b95ee83d73e4f28c77e7fe3e09b4c530a5
Xinjue Zou <hit_zou@126.com>
2022-08-29 14:50:10 +0800

    revise looping with conditional_variable

--- motion_planning_rviz_plugin/src/motion_planning_frame.cpp
+++ motion_planning_rviz_plugin/src/motion_planning_frame.cpp
@@ -145,6 +145,8 @@ MotionPlanningFrame::MotionPlanningFrame(MotionPlanningDisplay* pdisplay, rviz::
         }
         // disable stop button
         UiPtr->stop_button->setEnabled(false);
+
+        waypoints_tab_->notifyCv();
       }
     });
   });
